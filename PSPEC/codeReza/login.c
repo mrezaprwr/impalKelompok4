9d4ea1616ecd452843bfc420324a5bc435040170
Reza Siahaan <rsiahaan4@gmail.com>
2019-02-14 22:53:27 +0700

    Create login.c

---
+++ PSPEC/codeReza/login.c
@@ -0,0 +1,9 @@
+boolean loginAkun(char *username, char* password) {
+	bool valid=false; int i=0; 
+	while(valid==false) {
+		if(username==userData[i][0] && password==userData[i][1] {
+			valid=true;
+		}
+	}
+	return valid;
+}
